bfe13063c38751c72a9b1b0905a9a54c336201db
pds-cse <purbayandas121@gmail.com>
2025-05-27 18:49:24 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 // Developer Tools (Ctrl+Shift+I) -> Sources -> Left Pane (may be hidden) -> Snippets -> New Snipptes
 
 // paste and overwrite the following line with the cell content copied from gsheet
-var grades = [211011, 40.56, 211013, 49.67, 223011, 64.8, 223015, 74.97, 231012, 69.03, 233011];
+var grades = [21101183, 0, 21101317, 0, 22101256, 41.84, 22201860, 56.9, 22201878, 56.63, 22299416, 64.78, 22301165, 66.49, 22301511, 14.04, 23101219, 50.84, 23201003, 86.93, 23201103, 64.59, 23201145, 74.95, 23201152, 97.41, 23201341, 84.93, 23201360, 58.21, 23201430, 89.71, 23201458, 49.61, 23201654, 96.31, 23301075, 87.24, 23301125, 87.52, 23301550, 64.52, 24101399, 81.33, 24101659, 91.43, 24101673, 92.56, 23201445, 97.89];
 
 // Ctrl + Enter to run
 
-var data = document.querySelectorAll('input[placeholder="StudentId"]');
+var data = document.querySelectorAll('input[placeholder="Student"]');
 var marks_field = document.querySelectorAll('input[placeholder="Marks"]');
 var student_ids = [];
 
 for (let i=0; i<data.length; i++) {
-    student_ids.push(data[i].value);
+    student_ids.push(data[i].value.slice(0, 8));
     for(let j=0; j<grades.length; j+=2) {
         if(student_ids[i] == grades[j]) {
             marks_field[i].value = grades[j+1];
